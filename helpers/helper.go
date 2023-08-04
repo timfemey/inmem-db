f33698e757bfa9d99879b5689b9c3a431fb2761a
timfemey <isholaobafemi@gmail.com>
2023-08-04 19:13:14 +0100

    feat: Hashing, Compress & Decompress functions

--- helpers/helper.go
+++ helpers/helper.go
@@ -1,5 +1,68 @@
 package helper
 
-func Hash() {
+import (
+	"bytes"
+	"compress/gzip"
+	"encoding/gob"
+	"io"
 
+	"github.com/cespare/xxhash"
+)
+
+func Hash(key string) uint64 {
+	h := xxhash.Sum64String(key)
+	return h
+}
+
+func convertToByte(initialVal any) ([]byte, error) {
+	var byteBuf bytes.Buffer
+	encoder := gob.NewEncoder(&byteBuf)
+	err := encoder.Encode(initialVal)
+	if err != nil {
+		return []byte(""), err
+	}
+	return byteBuf.Bytes(), nil
+}
+
+func convertToAny(initialVal []byte) (any, error) {
+	buffer := bytes.NewBuffer(initialVal)
+	var decodedData any
+	decoder := gob.NewDecoder(buffer)
+	err := decoder.Decode(&decodedData)
+	if err != nil {
+		return "", err
+	}
+	return decodedData, nil
+}
+
+func Compress(data any) (bytes.Buffer, error) {
+	var compressedData bytes.Buffer
+	gzipScripter := gzip.NewWriter(&compressedData)
+	inBytes, err := convertToByte(data)
+	if err != nil {
+		return compressedData, err
+	}
+	_, err2 := gzipScripter.Write(inBytes)
+	if err2 != nil {
+		return compressedData, err
+	}
+	gzipScripter.Close()
+	return compressedData, nil
+}
+
+func DeCompress(compressedData bytes.Buffer) (any, error) {
+
+	gzipReader, err := gzip.NewReader(&compressedData)
+	if err != nil {
+		return "", err
+	}
+	decompressedData, err := io.ReadAll(gzipReader)
+	if err != nil {
+		return "", err
+	}
+	data, err := convertToAny(decompressedData)
+	if err != nil {
+		return "", err
+	}
+	return data, nil
 }
