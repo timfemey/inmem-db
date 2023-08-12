765cb2ddb998a78d0e9a062a8646b555966fe872
timfemey <isholaobafemi@gmail.com>
2023-08-12 08:10:17 +0100

    feat: Take in CLI flags

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"flag"
+	"inmem-db/store"
+)
+
+var initialSize int64
+var compress bool
+var hashtable store.HashTable
+
+func init() {
+	sizeArg := flag.Int("size", 1000, "How many Entries Memory DB should allow, Default is 1000")
+	compressArg := flag.Bool("compress", false, "Compress Values sent")
+
+	flag.Parse()
+
+	initialSize = int64(*sizeArg)
+	compress = *compressArg
+
+	hashtable = *store.NewHashTable(initialSize, compress)
+
+}
+
+func main() {
+
+}
